642f2f76ef0a3cbc2fd5db53ae6fb0e9cc713467
Royal Askarov <royalaskar42@gmail.com>
2026-01-10 23:10:41 +0400

    cm1.2

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,7 +5,9 @@ documentation
 import pandas as pd
 
 
-dictt = {"First": [0.0, 0.5, 1.0, 1.5],
-        "Second": ["one", "two", "three", "four"]}
+dictt = {
+        "First": [0.0, 0.5, 1.0, 1.5],
+        "Second": ["one", "two", "three", "four"]
+}
 
 df = pd.DataFrame(dictt, index=["A", "B", "C", "D"])
