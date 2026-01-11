ff880112a76acbc5bd81824ce038b67115961e8f
Royal Askarov <royalaskar42@gmail.com>
2026-01-11 15:39:03 +0400

    cm6

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -2,7 +2,6 @@
 """
 Module that contains a function to flip and switch a pd.DataFrame
 """
-import pandas as pd
 
 
 def flip_switch(df):
@@ -14,7 +13,7 @@ def flip_switch(df):
 
     Returns:
         the transformed pd.DataFrame
-    )"""
+    """
     # 1. Sort the data in reverse chronological order by its index
     # 2. Transpose the result so rows become columns and vice versa
     return df.sort_index(ascending=False).transpose()
