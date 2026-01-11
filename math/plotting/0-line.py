502ca637e7b1d7a548c592fd75d5f8de382c3864
Royal Askarov <royalaskar42@gmail.com>
2026-01-11 17:38:52 +0400

    cm0.3

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 
 
 def line():
-    """Plots y as a solid red line graph with the x-axis ranging from 0 to 10"""
+    """Plots y as a solid red line with x-axis from 0 to 10"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     # Plot y as a solid red line
-    # If x is not specified, matplotlib uses the index of y (0 to 10)
-    plt.plot(y, color='red', linestyle='solid')
+    plt.plot(y, 'r-')
 
     # Set the x-axis range from 0 to 10
     plt.xlim(0, 10)
