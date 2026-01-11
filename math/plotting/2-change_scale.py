a136d329029fd57660bbe27d71d4fb4ba20440bf
Royal Askarov <royalaskar42@gmail.com>
2026-01-11 17:43:27 +0400

    cm2.1

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-"""Module to plot a scatter plot of height vs weight"""
+"""Module to plot exponential decay with a logarithmic scale"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def scatter():
-    """Plots men's height vs weight as a magenta scatter plot"""
-    mean = [69, 0]
-    cov = [[15, 8], [8, 15]]
-    np.random.seed(5)
-    x, y = np.random.multivariate_normal(mean, cov, 2000).T
-    y += 180
+def change_scale():
+    """Plots the exponential decay of C-14 with a log y-axis"""
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
     plt.figure(figsize=(6.4, 4.8))
 
     # Plotting code
-    plt.scatter(x, y, color='magenta')
-    plt.xlabel('Height (in)')
-    plt.ylabel('Weight (lbs)')
-    plt.title("Men's Height vs Weight")
+    plt.plot(x, y)
+    plt.yscale('log')
+    plt.xlim(0, 28650)
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of C-14')
 
     plt.show()
