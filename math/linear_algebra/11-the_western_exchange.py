b7155346f97b6dc8e39279a9a9d18ff4a1ad1c03
royal <royal@gmail.com>
2026-02-01 23:38:03 +0400

    cm11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to transpose a numpy.ndarray
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a matrix
+    Args:
+        matrix: a numpy.ndarray
+    Returns:
+        a new numpy.ndarray that is the transpose of matrix
+    """
+    return matrix.transpose()
