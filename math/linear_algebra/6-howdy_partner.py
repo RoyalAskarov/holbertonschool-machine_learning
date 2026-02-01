e1b189422b17cff220b083f5fd806edf0b2eb6e5
royal <royal@gmail.com>
2026-02-01 22:34:57 +0400

    cm6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+sdfsaefwerf
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    adsf
+    :param arr1:
+    :param arr2:
+    :return:
+    """
+
+    return arr1 + arr2
