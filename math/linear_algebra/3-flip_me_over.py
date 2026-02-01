300545ea79b99e443187989abd99783fc185927a
royal <royal@gmail.com>
2026-02-01 21:54:28 +0400

    cm3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+adsflkj
+"""
+
+
+def matrix_transpose(matrix):
+    """
+    adfasfd
+    :param matrix:
+    :return:
+    """
+    return [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
