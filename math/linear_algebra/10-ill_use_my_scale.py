21781831057fd098780b328529f71dbf9eb8abcf
royal <royal@gmail.com>
2026-02-01 23:34:29 +0400

    cm10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to calculate the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray
+    Args:
+        matrix: A numpy.ndarray
+    Returns:
+        A tuple of integers representing the shape of the matrix
+    """
+    return matrix.shape
