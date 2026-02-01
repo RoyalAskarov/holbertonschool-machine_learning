b2b27e52ab14b0015ccddc8208b584a35af27c36
royal <royal@gmail.com>
2026-02-01 23:10:48 +0400

    cm8.1

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -23,7 +23,7 @@ def mat_mul(mat1, mat2):
     cols_mat1 = len(mat1[0])
     cols_mat2 = len(mat2[0])
 
-    result = [[0 for _ in range(cols_mat2)] for _ in range(rows_mat1)]
+    result=[[0 for _ in range(cols_mat2)] for _ in range(rows_mat1)]
 
     for i in range(rows_mat1):
         for j in range(cols_mat2):
