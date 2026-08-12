150c2ab11b226a749574587c70cb4174146aa32a
royal <royal@gmail.com>
2026-08-13 01:05:03 +0400

    cm

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
-""" Defines `load_frozen_lake`. """
-import gym
+"""Module to load Gymnasium's FrozenLake environment."""
+import gymnasium as gym
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
-    """
-    Loads the `FrozenLakeEnv` environment from OpenAI's `gym`.
+    """Loads the FrozenLakeEnv environment from Gymnasium.
 
-    desc: Either `None` or a list of lists containing a custom description of
-        the map to load for the environment.
-    map_name: Either `None` or a string containing the pre-made map to load.
-    is_slippery: A boolean to determine if the ice is slippery.
+    Args:
+        desc: list of lists containing a custom map description or None
+        map_name: string containing pre-made map name to load or None
+        is_slippery: boolean to determine if ice is slippery
 
-    Returns: The `FrozenLakeEnv` environment.
+    Returns:
+        The created Gymnasium FrozenLake environment
     """
     if desc is None and map_name is None:
-        map_name="8x8"
-    return gym.make(
-        'FrozenLake-v1', desc=desc, map_name=map_name, is_slippery=is_slippery)
+        map_name = '8x8'
+
+    env = gym.make(
+        'FrozenLake-v1',
+        desc=desc,
+        map_name=map_name,
+        is_slippery=is_slippery
+    )
+
+    return env
