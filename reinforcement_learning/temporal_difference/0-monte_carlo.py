0d7ece2b15806819f8676cb6d2294ff916be506e
royal <royal@gmail.com>
2026-09-01 23:50:36 +0400

    cm

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Performs Monte Carlo algorithm."""
+"""Performs the Monte Carlo algorithm."""
 
 import numpy as np
 
@@ -9,23 +9,26 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
     """Performs the Monte Carlo algorithm."""
     for _ in range(episodes):
         state, _ = env.reset()
-        episode = []
+        states = []
+        rewards = []
 
         for _ in range(max_steps):
+            states.append(state)
+
             action = policy(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            episode.append((state, reward))
-            state = next_state
+            state, reward, terminated, truncated, _ = env.step(action)
+            rewards.append(reward)
 
             if terminated or truncated:
                 break
 
-        for i, (state, _) in enumerate(episode):
+        for t in range(len(states)):
             G = 0
 
-            for _, reward in episode[i:]:
-                G = reward + gamma * G
+            for k in range(t, len(rewards)):
+                G += (gamma ** (k - t)) * rewards[k]
 
-            V[state] = V[state] + alpha * (G - V[state])
+            state = states[t]
+            V[state] = (1 - alpha) * V[state] + alpha * G
 
     return V
